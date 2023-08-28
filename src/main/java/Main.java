6094615bfb147fd1111084083a6384e3cff07937
Lukas Ejnarsson <lukkeejnarsson@gmail.com>
2023-08-28 16:27:03 +0200

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,39 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.NotFoundException;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+
+
+
+
+public class Main {
+
+    public static void generateQRcode(String data, String path, String charset, Map map, int h, int w)throws WriterException, IOException{
+        BitMatrix matrix = new MultiFormatWriter().encode(new String(data.getBytes(charset),charset),BarcodeFormat.QR_CODE,w,h);
+        MatrixToImageWriter.writeToFile(matrix,path.substring(path.lastIndexOf('.') +1), new File(path));
+    }
+    public static void main(String[] args) throws WriterException,IOException,NotFoundException {
+
+        String str = "https://www.arsenal.com/";
+
+        String path = "C:\\Users\\lukke\\OneDrive\\Images\\QRcode\\Quote.png";
+
+        String charset = "UTF-8";
+
+        Map<EncodeHintType,ErrorCorrectionLevel> hashMap = new HashMap<EncodeHintType,ErrorCorrectionLevel>();
+
+        hashMap.put(EncodeHintType.ERROR_CORRECTION,ErrorCorrectionLevel.L);
+
+        generateQRcode(str,path,charset,hashMap,200,200);
+
+        System.out.println("QR CODE successfully.");
+    }
+}
